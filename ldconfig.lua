ed016d9682368dac2b06ef026b4a3755d9013481
Stefan Eßer <se@FreeBSD.org>
2022-03-27 15:59:10 +0200

    Refactoring

--- ldconfig.lua
+++ ldconfig.lua
@@ -28,180 +28,197 @@ SUCH DAMAGE.
 --]]
 
 local lfs = require("lfs")
---local posix = require("posix")
 
-local hintsmagic = 1953392709
-local PATH_ELF_HINTS = "/var/run/ld-elf.so.hints"
 local PATH_ELF_HINTS = "/var/tmp/ld-elf.so.hints"
-local PATH_ELF32_HINTS = "/var/run/ld-elf32.so.hints"
+local PATH_ELF32_HINTS = "/var/tmp/ld-elf32.so.hints"
+
+local HINTSMAGIC = 1953392709
 local MAXFILESIZE = 16 * 1024
 
-local endianness
 local insecure
-local COMPAT_LIBS = {}
-local DIRS = {}
+local endianness
+local COMPAT_DIRS
 
+-- return a table of compat library backup directories
 local function get_compat_dirs()
-	local compatlibs = { -- should use sysctl("usr.compat_libs")
+	local compat_dirs = { -- should use sysctl("user.compatlibs_path")
 		"/usr/local/lib/compat/pkg",
 		--"usr/local/lib32/compat",
 		}
-	return compatlibs
+	local compatdir_table = {}
+	for i = 1, #compat_dirs do
+		compatdir_table[compat_dirs[i]] = i
+	end
+	return compatdir_table
 end
 
+-- return position of a directory in the compat directory table
 local function compat_path_idx(dir, trusted)
-	COMPAT_LIBS = COMPAT_LIBS or get_compat_dirs()
-	for i = 1, #COMPAT_LIBS do
-		if dir == COMPAT_LIBS[i] then
-			return i
+	if not COMPAT_DIRS then
+		if not trusted then
+			COMPAT_DIRS = get_compat_dirs()
+		else
+			COMPAT_DIRS = {}
 		end
 	end
-	return -1
+	return COMPAT_DIRS[dir] or -1
 end
 
-local function add_dir(name, trusted)
+-- return position where to insert new search path entry
+local function insert_at(dirs, name, trusted)
+	local pos = #dirs + 1
+	--[[
+	local compat_idx = compat_path_idx(name, trusted)
+	for i = 1, #dirs do
+		if name == dirs[i] then
+			return
+		end
+		if compat_idx < compat_path_idx(dirs[i], trusted) then
+			pos = pos - 1
+		end
+	end
+	--]]
+	return pos
+end
+
+-- check library directory ownership and permissions
+local function validate_dir(name)
+	local attr, errmsg = lfs.attributes(name)
+	if attr then
+		local mode = tonumber(attr.permissions, 8)
+		if attr.uid ~= 0 then
+			errmsg = "ignoring directory not owned by root"
+		elseif mode & 2 ~= 0 then
+			errmsg = "ignoring world-writable directory"
+		elseif mode & (2 << 3) ~= 0 then
+			errmsg = "ignoring group-writable directory"
+		else
+			errmsg = nil
+		end
+	end
+	if errmsg then
+		return nil, name .. ": " .. errmsg
+	else
+		return true
+	end
+end
+
+-- add library directory to the search path
+local function add_dir(dirs, name, trusted)
 	-- Do some security checks
 	if not trusted and not insecure then
-		local attr, errmsg = lfs.attributes(name)
-		if attr then
-			local mode = tonumber(attr.permissions, 8)
-			if attr.uid ~= 0 then
-				print(name, ": ignoring directory not owned by root")
-			elseif mode & 2 ~= 0 then
-				print(name, ": ignoring world-writable directory")
-			elseif mode & (2 << 3) ~= 0 then
-				print(name, ": ignoring group-writable directory")
-			else
-				local compat_idx = compat_path_idx(name, trusted)
-				local insert_at = #DIRS + 1
-				for i = 1, #DIRS do
-					if name == DIRS[i] then
-						return
-					end
-					if compat_idx < compat_path_idx(DIRS[i], trusted) then
-						insert_at = insert_at - 1
-					end
-				end
-				table.insert(DIRS, insert_at, name)
-			end
-		else
+		local secure, errmsg = validate_dir(name)
+		if not secure then
 			print(errmsg .. ": " .. name)
+			return
 		end
+	end
+	local pos = insert_at(dirs, name, trusted)
+	if pos then
+		table.insert(dirs, pos, name)
 	end
 end
 
 local function abort(msg)
-	print (msg)
+	local program = string.match(arg[0], "[^/]+$")
+	print (program .. ": " .. msg)
 	os.exit(1)
 end
 
-local function shlibs(path)
+-- return table with full pathnames of all files with names like "libc.so.1" in dirs
+local function all_libs(dirs)
 	local libs = {}
-	for name in lfs.dir(path) do
-		if string.match(name, ".*%.so.[%d]+$") then
-			libs[#libs+1] = path .. "/" .. name
+	for _, dir in ipairs(dirs) do
+		for name in lfs.dir(dir) do
+			if string.match(name, ".*%.so.[%d]+$") then
+				libs[#libs+1] = dir .. "/" .. name
+			end
 		end
 	end
 	return libs
 end
 
-local function parsehdr(data, forced_endianness)
+-- parse hintsfile header and return path and detected endianness
+local function parse_hdr(data)
+	local file_endianness
 	local magic, version, strtab, strsize, dirlist, dirlistlen =
 		string.unpack("<I4 I4 I4 I4 I4 I4", data)
-	if magic == hintsmagic then
-		endianness = "<"
+	if magic == HINTSMAGIC then
+		file_endianness = "<"
 	else
 		magic, version, strtab, strsize, dirlist, dirlistlen =
 			string.unpack(">I4 I4 I4 I4 I4 I4", data)
-		if magic ~= hintsmagic then
-			abort("bad magic number")
+		if magic == HINTSMAGIC then
+			file_endianness = ">"
+		else
+			return nil, "bad magic number"
 		end
-		endianness = ">"
 	end
-	if forced_endianness and forced_endianness ~= endianness then
-		abort("conflicting endianness requested")
+	if endianness and file_endianness and endianness ~= file_endianness then
+		return nil, "conflicting endianness requested"
 	end
+	endianness = file_endianness
 	if version ~= 1 then
-		abort("invalid hints file version " .. version)
+		return nil, "invalid hints file version " .. version
 	end
 	local offset = strtab + dirlist
-	local path = string.sub(data, offset + 1, offset + dirlistlen)
+	return string.sub(data, offset + 1, offset + dirlistlen)
+end
+
+-- parse the hints file header with automatic endianness detection
+local function add_hints_dirs(data)
+	local path, errmsg = parse_hdr(data)
+	if not path then
+		return nil, errmsg
+	end
+	local dirs = {}
 	for dir in string.gmatch(path, "([^:]+):?") do
-		add_dir(dir, false)
+		add_dir(dirs, dir, true)
 	end
+	return dirs
 end
 
-local function readhints(filename)
+-- read hints from file and return dirs and endianess
+local function readhints(filename, must_exist)
 	local hintsfile = io.open(filename, "rb")
-	if not hintsfile then
-		abort("missing hintsfile " .. filename)
-	end
-	local data = hintsfile:read("a")
-	hintsfile:close()
-	parsehdr(data)
-end
-
-local function list_elf_hints(hintsfile)
-	readhints(hintsfile)
-	print (hintsfile .. ":")
-	print ("\tsearch directories: " .. table.concat(DIRS, ":"))
-	local all_libs = {}
-	for _, dir in ipairs(DIRS) do
-		local libs = shlibs(dir)
-		table.move(libs, 1 , #libs, #all_libs + 1, all_libs)
-	end
-	local lcount = 0
-	for _, lib in ipairs(all_libs) do
-		local lopt, version = string.match(lib, ".*/lib([^/]+).so.(%d+)")
-		if lopt then
-			print ("\t" .. tostring(lcount) .. ":-l" .. lopt .. "." .. version .. " => " .. lib)
-			lcount = lcount + 1
-		end
-	end
-end
-
-local function read_dirs_from_file(listfile)
-	local fp = io.open(listfile, "r")
-	if not fp then
-		abort("file not found: " .. listfile)
-	end
-	local linenum = 0
-	for buf in fp:read("l") do
-		linenum = linenum + 1
-		local dir, rest = string.match(buf, "%s*([^#]%S+)%s*(.*)")
-		if dir and #dir > 0 then
-			if rest then
-				print(listfile .. ":" .. linenum .. ": trailing characters ignored")
-			end
-			add_dir(dir, false);
-		end
-	end
-	fp:close()
-end
-
-local function read_elf_hints(hintsfile, must_exist)
-	local fd = io.open(hintsfile, "r")
-	if not fd then
-		if must_exist then
-			abort('Cannot open "' .. hintsfile .. '"')
-		end
+	if hintsfile then
+		local data = hintsfile:read("a")
+		hintsfile:close()
+		return add_hints_dirs(data)
+	elseif must_exist then
+		return nil, filename .. " not found"
 	else
-		local attr = lfs.attributes(hintsfile)
-		if not attr then
-			abort('Cannot stat "'..  hintsfile .. '"')
-		end
-		if attr.size > MAXFILESIZE then
-			abort('"' .. hintsfile .. '" is unreasonably large')
-		end
-		local hintsdata = fd:read("a")
-		parsehdr(hintsdata)
-		fd:close()
+		return {}
 	end
 end
 
-local function generatehints()
-	local path = table.concat(DIRS, ":")
-	local magic = hintsmagic
+-- check existence, permissions(?), and size of the hints file
+local function validate_hintsfile(name)
+	local attr, errmsg = lfs.attributes(name)
+	if errmsg then
+		return nil, errmsg
+	end
+	if attr.size > MAXFILESIZE then
+		return nil, "file is unreasonably large (" .. attr.size .. " bytes)"
+	end
+end
+
+-- fetch list of library directories from hints file
+local function read_elf_hints(dirs, hintsfile, must_exist)
+local valid, errmsg = validate_hintsfile(hintsfile)
+	if valid then
+		dirs, errmsg = readhints(hintsfile, must_exist)
+	end
+	if errmsg then
+		return nil, errmsg
+	end
+	return dirs
+end
+
+-- generate the data to write to a new hints file
+local function generatehints(dirs)
+	local path = table.concat(dirs, ":")
+	local magic = HINTSMAGIC
 	local version = 1
 	local strtab = 128
 	local strsize = #path + 1
@@ -214,38 +231,101 @@ local function generatehints()
 	return hdr
 end
 
-local function write_elf_hints(hintsfile)
+-- write a new hints file via a temporary file
+local function write_elf_hints(hintsfile, dirs)
 	local tmpname = os.tmpname()
 	local destdir = string.match(hintsfile, "^(.*)/")
 	tmpname = destdir .. "/" .. string.match(tmpname, "[^/]+$")
 	local fb = io.open(tmpname, "w")
-	fb:write(generatehints())
-	fb:close()
-	os.rename(tmpname, hintsfile)
+	if not fb then
+		return nil, "failed to open output file " .. hintsfile
+	else
+		fb:write(generatehints(dirs))
+		fb:close()
+		if not os.rename(tmpname, hintsfile) then
+			return nil, "failed to open output file " .. hintsfile
+		end
+	end
+	return true
 end
 
-local function update_elf_hints(hintsfile, merge, force_be, force_le)
+-- set the endianess to be used accessing the hints file
+local function set_endianness(force_be, force_le)
 	if force_be then
 		endianness = ">"
 	elseif force_le then
 		endianness = "<"
+	else
+		endianness = nil
 	end
-	if merge then
-		read_elf_hints(hintsfile, false);
-	end
-	for i = 1, #arg do
-		local attr, errmsg = lfs.attributes(arg[i])
-		if not attr then
-			print("warning: " .. arg[i] .. ": " .. errmsg)
-		elseif attr.mode == "file" then
-			read_dirs_from_file(arg[i])
-		else
-			add_dir(arg[i], false);
-		end
-	end
-	write_elf_hints(hintsfile);
 end
 
-update_elf_hints(PATH_ELF_HINTS, true, false, false)
+-- read a list of directories from a file
+local function read_dirs_from_file(dirs, listfile)
+	local fp = io.open(listfile, "r")
+	if not fp then
+		abort("file not found: " .. listfile)
+	end
+	local linenum = 0
+	for buf in fp:read("l") do
+		linenum = linenum + 1
+		local dir, rest = string.match(buf, "%s*([^#]%S+)%s*(.*)")
+		if dir and #dir > 0 then
+			if rest then
+				print(listfile .. ":" .. linenum .. ": trailing characters ignored")
+			end
+			add_dir(dirs, dir, false);
+		end
+	end
+	fp:close()
+end
+
+-- update hints file
+local function update_elf_hints(hintsfile, newdirs, merge)
+	local dirs = {}
+	if merge then
+		read_elf_hints(dirs, hintsfile, false);
+	end
+	for _, dir in ipairs(newdirs) do
+		local attr, errmsg = lfs.attributes(dir)
+		if not attr then
+			print("warning: " .. dir .. ": " .. errmsg)
+		elseif attr.mode == "file" then
+			read_dirs_from_file(dirs, dir)
+		else
+			add_dir(dirs, dir, false);
+		end
+	end
+	local success, errmsg = write_elf_hints(hintsfile, dirs)
+	if not success then
+		abort(errmsg)
+	end
+end
+
+-- list all directories in search order
+local function list_elf_hints(hintsfile)
+	local dirs, errmsg = readhints(hintsfile)
+	if not dirs then
+		abort(errmsg)
+	end
+	print (hintsfile .. ":")
+	print ("\tsearch directories: " .. table.concat(dirs, ":"))
+	local lcount = 0
+	for _, lib in ipairs(all_libs(dirs)) do
+		local lopt, version = string.match(lib, ".*/lib([^/]+).so.(%d+)")
+		if lopt and version then
+			print ("\t" .. tostring(lcount) .. ":-l" .. lopt .. "." .. version .. " => " .. lib)
+			lcount = lcount + 1
+		end
+	end
+end
+
+---------------------------------------------------------------
+set_endianness(true, false)
+update_elf_hints(PATH_ELF_HINTS, arg, false and true)
+
+set_endianness(false, false)
 list_elf_hints(PATH_ELF32_HINTS)
+
+set_endianness(false, false)
 --list_elf_hints("/var/run/ld-elf.so.hints")
